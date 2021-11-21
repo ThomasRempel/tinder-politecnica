45a5dc33f6587b3b9e69588ff5750c0d66a5b499
MikaeldaSilvaS <silmike14@yahoo.com.br>
2021-11-21 18:50:10 -0300

    Adicionando exceptions e trocando caracter de pergunta de gênero do usuário

--- src/controlador/Controlador.java
+++ src/controlador/Controlador.java
@@ -88,8 +88,7 @@ public class Controlador {
 
         if (opcaoEscolhida == 1) {
             editarPerfil();
-        }
-        else if(opcaoEscolhida == 2) {
+        } else if (opcaoEscolhida == 2) {
             iniciaProgramaPrincipal();
         }
     }
@@ -102,23 +101,17 @@ public class Controlador {
 
             if (opcaoEscolhida == 1) {
                 editarNome();
-            }
-            else if (opcaoEscolhida == 2) {
+            } else if (opcaoEscolhida == 2) {
 
-            }
-            else if (opcaoEscolhida == 3) {
+            } else if (opcaoEscolhida == 3) {
 
-            }
-            else if (opcaoEscolhida == 4) {
+            } else if (opcaoEscolhida == 4) {
 
-            }
-            else if (opcaoEscolhida == 5) {
+            } else if (opcaoEscolhida == 5) {
 
-            }
-            else if (opcaoEscolhida == 6) {
+            } else if (opcaoEscolhida == 6) {
 
-            }
-            else if (opcaoEscolhida == 7) {
+            } else if (opcaoEscolhida == 7) {
 
             }
         }
@@ -143,107 +136,145 @@ public class Controlador {
         sc.nextLine();
         String sexo = sc.nextLine();
 
-        if (sexo.toLowerCase().equals("m")) {
+        if (sexo.toLowerCase().equals("h")) {
             cadastrarHomem();
-        } else if (sexo.toLowerCase().equals("f")) {
+        } else if (sexo.toLowerCase().equals("m")) {
             cadastrarMulher();
         }
     }
 
     private void cadastrarHomem() {
-        String nome;
+        System.out.println( "\n" + "Iniciando cadastro..." + "\n");
+        pause(3500);
+        try {
+            System.out.print("Digite seu nome:");
+            String nome = sc.nextLine();
+            validaCaracter(nome);
 
-        System.out.print("Digite seu nome:");
-        nome = sc.nextLine();
-        //verificaCaracter(nome);
+            System.out.print("Digite sua senha: ");
+            String senha = sc.nextLine();
 
-        System.out.print("Digite sua senha: ");
-        String senha = sc.nextLine();
+            System.out.print("Digite sua idade: ");
+            int idade = sc.nextInt();
+            verificaNumero(idade);
 
-        System.out.print("Digite sua idade: ");
-        int idade = sc.nextInt();
-        //verificaNumero(idade);
+            System.out.print("Digite seu curso: ");
+            sc.nextLine();
+            String curso = sc.nextLine();
+            validaCaracter(curso);
 
-        System.out.print("Digite seu curso: ");
-        sc.nextLine();
-        String curso = sc.nextLine();
-        //verificaCaracter(curso);
+            System.out.print("Digite seu período: ");
+            int periodo = sc.nextInt();
+            verificaNumero(periodo);
 
-        System.out.print("Digite seu período: ");
-        int periodo = sc.nextInt();
+            listaGostos();
+            System.out.println("Digite o que você mais gosta: ");
+            int gosto1 = sc.nextInt();
+            System.out.println("Digite o que você mais gosta: ");
+            int gosto2 = sc.nextInt();
+            List<String> listaDeGostos = new ArrayList<>();
+            listaDeGostos.add(String.valueOf(gosto1));
+            listaDeGostos.add(String.valueOf(gosto2));
 
-        listaGostos();
-        System.out.println("Digite o que você mais gosta: ");
-        int gosto1 = sc.nextInt();
-        System.out.println("Digite o que você mais gosta: ");
-        int gosto2 = sc.nextInt();
-        List<String> listaDeGostos = new ArrayList<>();
-        listaDeGostos.add(String.valueOf(gosto1));
-        listaDeGostos.add(String.valueOf(gosto2));
+            System.out.print("Digite algo sobre você: ");
+            sc.nextLine();
+            String sobreMim = sc.nextLine();
 
-        System.out.print("Digite algo sobre você: ");
-        sc.nextLine();
-        String sobreMim = sc.nextLine();
+            String cadastro = nome + ";" + senha + ";" + idade + ";" + curso + ";" + periodo + ";" + gosto1 + ";" + gosto2 + ";" + sobreMim;
 
-        String cadastro = nome + ";" + senha + ";" + idade + ";" + curso + ";" + periodo + ";" + gosto1 + ";" + gosto2 + ";" + sobreMim;
+            ControladorTxt.ControladorTxt(cadastro, "C:\\Windows\\Temp\\perfisMasc.txt");
+            ControladorTxt controladorTxt = new ControladorTxt();
+            UsuarioHomem usuarioHomem = new UsuarioHomem(nome, senha, idade, curso, periodo, listaDeGostos, sobreMim);
 
-        ControladorTxt controladorTxt = new ControladorTxt();
-        ControladorTxt.ControladorTxt(cadastro, "C:\\Windows\\Temp\\perfisMasc.txt");
-
-        UsuarioHomem usuarioHomem = new UsuarioHomem(nome, senha, idade, curso, periodo, listaDeGostos, sobreMim);
-
-        System.out.println("Logou com sucesso!");
-
-    }
-    /*private void verificaCaracter(String objeto) throws ExcecaoCaracter {
-        KeyEvent evt = null;
-        String caracteres = "0987654321";// lista de caracters que não devem ser aceitos
-
-        if (caracteres.contains(evt.getKeyChar() + "")) {// se o character que gerou o
-            throw new ExcecaoCaracter("Dígito inválido. Use apenas caracteres não numéricos");
+            System.out.println("Logou com sucesso!");
+        } catch (ExcecaoCaracter e) {
+            e.getMessage();
+        } catch (ExcecaoNumero e1) {
+            e1.getMessage();
+        } catch (Exception e2){
+            System.out.println("Somente caracteres númericos");
+            cadastrarHomem();
         }
     }
 
-    private void verificaNumero(int objeto) throws ExcecaoNumero{
+    private void cadastrarMulher(){
+        System.out.println( "\n" + "Iniciando cadastro..." + "\n");
+        pause(5000);
+        try {
+            System.out.print("Digite seu nome:");
+            String nome = sc.nextLine();
+            validaCaracter(nome);
+
+            System.out.print("Digite sua senha: ");
+            String senha = sc.nextLine();
+
+            System.out.print("Digite sua idade: ");
+            int idade = sc.nextInt();
+            verificaNumero(idade);
+
+            System.out.print("Digite seu curso: ");
+            sc.nextLine();
+            String curso = sc.nextLine();
+            validaCaracter(curso);
+
+            System.out.print("Digite seu período: ");
+            int periodo = sc.nextInt();
+            verificaNumero(periodo);
+
+            listaGostos();
+            System.out.println("Digite o que você mais gosta: ");
+            int gosto1 = sc.nextInt();
+            System.out.println("Digite o que você mais gosta: ");
+            int gosto2 = sc.nextInt();
+            List<String> listaDeGostos = new ArrayList<>();
+            listaDeGostos.add(String.valueOf(gosto1));
+            listaDeGostos.add(String.valueOf(gosto2));
+
+            System.out.print("Digite algo sobre você: ");
+            sc.nextLine();
+            String sobreMim = sc.nextLine();
+
+            String cadastro = nome + ";" + senha + ";" + idade + ";" + curso + ";" + periodo + ";" + gosto1 + ";" + gosto2 + ";" + sobreMim;
+
+            ControladorTxt.ControladorTxt(cadastro, "C:\\Windows\\Temp\\perfisMasc.txt");
+            ControladorTxt controladorTxt = new ControladorTxt();
+            UsuarioHomem usuarioHomem = new UsuarioHomem(nome, senha, idade, curso, periodo, listaDeGostos, sobreMim);
+
+            System.out.println("Logou com sucesso!");
+        } catch (ExcecaoCaracter e) {
+            System.out.println(e.getMessage());
+            cadastrarMulher();
+        } catch (ExcecaoNumero e1) {
+            System.out.println(e1.getMessage());
+            cadastrarMulher();
+        } catch (Exception e2){
+            System.out.println("Só é válido caracteres númericos");
+            sc.nextLine();
+            cadastrarMulher();
+        }
+    }
+
+    public void validaCaracter(String objeto) throws ExcecaoCaracter {
+        for (int i = 0; i < objeto.length(); i++) {
+            char caracterDoObjeto = objeto.charAt(i);
+            if ((!Character.isLetter(caracterDoObjeto)) && !(caracterDoObjeto == ' ')) {
+                throw new ExcecaoCaracter("Dígito inválido. Use apenas caracteres não numéricos");
+            }
+        }
+    }
+
+    private void verificaNumero(int objeto) throws ExcecaoNumero {
         if (String.valueOf(objeto) == null || String.valueOf(objeto).isEmpty()) {
             throw new ExcecaoNumero("Dígito inválido. Use apenas caracteres numéricos");
-
         }
-    }*/
+    }
 
-
-    private void cadastrarMulher() {
-        System.out.print("Digite seu nome:");
-        String nome = sc.nextLine();
-        System.out.print("Digite sua senha:");
-        String senha = sc.nextLine();
-        System.out.print("Digite sua idade:");
-        int idade = sc.nextInt();
-        System.out.print("Digite seu curso:");
-        sc.nextLine();
-        String curso = sc.nextLine();
-        System.out.print("Digite seu período:");
-        int periodo = sc.nextInt();
-
-        listaGostos();
-        System.out.println("Digite o que você mais gosta: ");
-        int gosto1 = sc.nextInt();
-        System.out.println("Digite o que você mais gosta: ");
-        int gosto2 = sc.nextInt();
-        List<String> listaDeGostos = new ArrayList<>();
-        listaDeGostos.add(String.valueOf(gosto1));
-        listaDeGostos.add(String.valueOf(gosto2));
-
-        System.out.print("Digite algo sobre você: ");
-        sc.nextLine();
-        String sobreMim = sc.nextLine();
-
-        String cadastro = nome + ";" + senha + ";" + idade + ";" + curso + ";" + periodo + ";" + gosto1 + ";" + gosto2 + ";" + sobreMim;
-        ControladorTxt controladorTxt = new ControladorTxt();
-        ControladorTxt.ControladorTxt(cadastro, "C:\\Windows\\Temp\\perfisFem.txt");
-
-        UsuariaMulher usuariaMulher = new UsuariaMulher(nome, senha, idade, curso, periodo, listaDeGostos, sobreMim);
-        System.out.println("Logou com sucesso!");
+    public static void pause(int ms) {
+        try {
+            Thread.sleep(ms);
+        } catch (InterruptedException e) {
+            System.err.format("IOException: %s%n", e);
+        }
     }
 
     private void listaGostos() {
@@ -266,7 +297,7 @@ public class Controlador {
         if (opcaoEscolhida == 1) {
             System.out.println("Parabéns! Aí está o amor da sua vida!");
             return true;
-        }else if(opcaoEscolhida == 3){
+        } else if (opcaoEscolhida == 3) {
             iniciaProgramaPrincipal();
         }
         return false;
--- src/principal/UI.java
+++ src/principal/UI.java
@@ -41,7 +41,7 @@ public class UI {
 
     public void homemOuMulher() {
         System.out.println("Qual o seu sexo?");
-        System.out.print("Digite \"M\" para homem ou \"F\" para mulher: ");
+        System.out.print("Digite \"H\" para homem ou \"M\" para mulher: ");
     }
 
     public void oqueVoceEstaBuscando(){
